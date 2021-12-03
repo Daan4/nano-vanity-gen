ae297172404d355ff1714c786ade19863f1cd9ab
Daan4 <Daan4@users.noreply.github.com>
2021-12-03 20:43:59 +0100

    Initial implementation

---
+++ src/main.rs
@@ -0,0 +1,120 @@
+use bitvec::prelude::*;
+use blake2b_simd::{Hash, Params};
+use byteorder::{BigEndian, WriteBytesExt};
+use ed25519_dalek::{PublicKey, SecretKey};
+use once_cell::sync::Lazy;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+
+const STARTS_WITH: &str = "daan";
+
+type Seed = [u8; 32];
+
+fn main() {
+    let rng = &mut ChaCha20Rng::from_entropy();
+
+    // todo validate if pattern can actually be matched
+
+    loop {
+        let seed = generate_random_seed(rng);
+        let private_key = derive_private_key(seed, 0);
+        let public_key = derive_public_key(private_key);
+        let address = derive_address(public_key);
+        println!("{}", address);
+        if address[6..].starts_with(STARTS_WITH) {
+            println!("{}", bytes_to_hexstring(&seed));
+            break;
+        }
+    }
+}
+
+/// Benchmark each function
+fn benchmark() {
+
+}
+
+/// Generate random seed
+fn generate_random_seed(rng: &mut ChaCha20Rng) -> Seed {
+    let mut seed: Seed = [0; 32];
+    for i in 0..32 {
+        seed[i] = rng.gen_range(0..16) << 4 | rng.gen_range(0..16);
+    }
+    seed
+}
+
+/// Derive private key from seed and index
+fn derive_private_key(seed: Seed, index: u32) -> Hash {
+    let mut wtr = vec![];
+    wtr.write_u32::<BigEndian>(index).unwrap();
+    Params::new()
+        .hash_length(32)
+        .to_state()
+        .update(&seed)
+        .update(&wtr)
+        .finalize()
+}
+
+/// Derive public key from private key
+fn derive_public_key(private_key: Hash) -> PublicKey {
+    PublicKey::from(&SecretKey::from_bytes(private_key.as_bytes()).unwrap())
+}
+
+ /// Derive address from public key
+fn derive_address(public_key: PublicKey) -> String {
+    // Code based on Feeless project implementation
+    let mut address = String::with_capacity(65);
+    address.push_str("nano_");
+
+    const PKP_LEN: usize = 4 + 8 * 32;
+    const PKP_CAPACITY: usize = 4 + 8 * 32 + 4;
+    let mut bits: BitVec<Msb0, u8> = BitVec::with_capacity(PKP_CAPACITY);
+    let pad: BitVec<Msb0, u8> = bitvec![Msb0, u8; 0; 4];
+    bits.extend_from_bitslice(&pad);
+    bits.extend_from_raw_slice(public_key.as_bytes());
+    debug_assert_eq!(bits.capacity(), PKP_CAPACITY);
+    debug_assert_eq!(bits.len(), PKP_LEN);
+    let public_key_part = encode_nano_base_32(&bits);
+    address.push_str(&public_key_part);
+
+    let result = Params::new()
+        .hash_length(5)
+        .to_state()
+        .update(public_key.as_bytes())
+        .finalize();
+    let bits: BitVec<Msb0, u8> = BitVec::from_iter(result.as_bytes().iter().rev());
+    let checksum = encode_nano_base_32(&bits);
+    address.push_str(&checksum);
+    address
+}
+
+// Function based on Feeless project implementation
+const ALPHABET: &str = "13456789abcdefghijkmnopqrstuwxyz";
+static ALPHABET_VEC: Lazy<Vec<char>> = Lazy::new(|| ALPHABET.chars().collect());
+const ENCODING_BITS: usize = 5;
+
+fn encode_nano_base_32(bits: &BitSlice<Msb0, u8>) -> String {
+    debug_assert_eq!(
+        bits.len() % ENCODING_BITS,
+        0,
+        "BitSlice must be divisible by 5"
+    );
+    let mut s = String::new(); // TODO: with_capacity
+    for idx in (0..bits.len()).step_by(ENCODING_BITS) {
+        let chunk: &BitSlice<Msb0, u8> = &bits[idx..idx + ENCODING_BITS];
+        let value: u8 = chunk.load_be();
+        let char = ALPHABET_VEC[value as usize];
+        s.push(char);
+    }
+    s
+}
+
+const HEX: [&str; 16] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+
+fn bytes_to_hexstring(bytes: &[u8]) -> String {     
+    let mut buf = String::new();
+    for x in bytes.iter() {
+        buf += HEX[(*x >> 4) as usize];
+        buf += HEX[0x0F & *x as usize];
+    }
+    buf
+}
